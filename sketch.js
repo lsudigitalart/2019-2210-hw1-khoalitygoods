ff6b9d57b15ecbde2cbf3b0ad501fbd61e2e40b6
khoa <kbui9@lsu.edu>
2019-08-29 14:43:34 -0500

    Homework

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 function setup() {
-    creatCanvas (800, 600);
+    createCanvas (800, 600);
     //  background (255, 255, 255); 
     
-    fill (0);
+    //fill (0);
 
-    noStroke ();
+    //noStroke ();
     ellipse(100, 300, 100, 100);
 
 
     strokeWeight (10)
-    noStroke ();
+    //noStroke ();
     stroke (255);
     ellipse(100, 300, 50, 50);
 
