1fa151c2eafffbc1f09f515b65f437bc90f54db5
khoa <kbui9@lsu.edu>
2019-09-03 12:29:29 -0500

    Assignment1

--- sketch.js
+++ sketch.js
@@ -2,10 +2,17 @@ function setup() {
     createCanvas (800, 600);
     //  background (255, 255, 255); 
     
-    //fill (0);
+    fill (0);
+    ellipse(395, 395, 300, 300);
+     
+    fill (256);
+     ellipse(395, 395, 250, 250);
 
+     fill (0)
+     ellipse(390, 390, 100, 100);
     //noStroke ();
     ellipse(100, 300, 100, 100);
+
 
 
     strokeWeight (10)
